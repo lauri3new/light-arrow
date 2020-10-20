ea28c3fa76a06a2deb2544396b14b362de1fa53c
lauri3new <laurence@wearepercent.com>
2020-10-20 05:40:05 +0100

    perf

--- src/test/perf.test.ts
+++ src/test/perf.test.ts
@@ -11,7 +11,7 @@ it('should not stack overflow', async () => {
   expect(result).toEqual(100001)
 })
 
-it('should be close to speed of promises', async () => {
+it('should flatMap close to speed of promises', async () => {
   const p1 = performance.now()
   let a = sArrow<{}, never, number>(async () => Right(1))
   for (let i = 0; i < 1000000; i += 1) {
@@ -29,4 +29,24 @@ it('should be close to speed of promises', async () => {
   const promiseRunTime = p4 - p3
   const arrowRunTime = p2 - p1
   expect(arrowRunTime).toBeLessThan(promiseRunTime * 1.25)
+})
+
+it('should map close to speed of promises', async () => {
+  const p1 = performance.now()
+  let a = sArrow<{}, never, number>(async () => Right(1))
+  for (let i = 0; i < 1000000; i += 1) {
+    a = a.map((c: number) => c + 1)
+  }
+  await a.runAsPromise({})
+  const p2 = performance.now()
+  const p3 = performance.now()
+  let b = Promise.resolve(1)
+  for (let i = 0; i < 1000000; i += 1) {
+    b = b.then((c: number) => c + 1)
+  }
+  await a.runAsPromise({})
+  const p4 = performance.now()
+  const promiseRunTime = p4 - p3
+  const arrowRunTime = p2 - p1
+  expect(arrowRunTime).toBeLessThan(promiseRunTime * 1.20)
 })
